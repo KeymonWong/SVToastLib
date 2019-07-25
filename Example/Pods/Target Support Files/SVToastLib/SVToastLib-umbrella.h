adfae930a3901e897d94fd0f4292f2009b1f285b
huangqimeng <hqm_vip@163.com>
2019-07-25 11:26:40 +0800

    update

--- Example/Pods/Target Support Files/SVToastLib/SVToastLib-umbrella.h
+++ Example/Pods/Target Support Files/SVToastLib/SVToastLib-umbrella.h
@@ -10,10 +10,7 @@
 #endif
 #endif
 
-#import "SVIndefiniteAnimatedView.h"
-#import "SVProgressAnimatedView.h"
 #import "SVProgressHUD.h"
-#import "SVRadialGradientLayer.h"
 #import "SVToast.h"
 
 FOUNDATION_EXPORT double SVToastLibVersionNumber;
